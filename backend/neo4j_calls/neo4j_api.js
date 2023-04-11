3eccfe4fb49ef876edca0252da9fbd499920a761
Aaron Putterman <abp6318@rit.edu>
2023-04-11 15:22:49 -0400

    Some small changes here and there

--- backend/neo4j_calls/neo4j_api.js
+++ backend/neo4j_calls/neo4j_api.js
@@ -3,16 +3,15 @@ let neo4j = require('neo4j-driver');
 const _ = require('lodash');
 let driver = neo4j.driver(process.env.NEO4JURI, neo4j.auth.basic(process.env.NEO4JUSERNAME, process.env.NEO4JPASSWORD));
 
-exports.get_num_nodes = async function () {
-    let session = driver.session();
-    const num_nodes = await session.run('MATCH (n) RETURN n', {});
-    session.close();
-    return (!num_nodes ? 0 : num_nodes.records.length);
-};
-
 exports.get_all_nodes = async function () {
     let session = driver.session();
-    const result = await session.run('MATCH (n) RETURN n', {});
+    const result = await session.run(
+        'MATCH (t:TVShow)' +
+        'OPTIONAL MATCH (t)-[:SEASON_OF]->(s:Season)' +
+        'OPTIONAL MATCH (s)-[:EPISODE_OF]->(e:Episode)' +
+        'RETURN t, s, e',
+        {}
+    );    
     session.close();
     return result;
 };
@@ -20,7 +19,9 @@ exports.get_all_nodes = async function () {
 exports.get_tvshow = async function (title) {
     let session = driver.session();
     const result = await session.run(
-        'MATCH (t:TVShow {title: "'+title+'"})-[:SEASON_OF]->(s:Season)-[:EPISODE_OF]->(e:Episode)' +
+        'MATCH (t:TVShow {title: "'+title+'"})' +
+        'OPTIONAL MATCH (t)-[:SEASON_OF]->(s:Season)' +
+        'OPTIONAL MATCH (s)-[:EPISODE_OF]->(e:Episode)' +
         'RETURN t, s, e',
         {}
     );
--- backend/routes/default.js
+++ backend/routes/default.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * http://localhost:3001/
+ * 
+ * Default route
+ * @params none
+ */
+// TODO: Add some sort of redirect?
+// TODO: Logging
 router.get('/', async (req, res) => {
     res.send("Try /tvshows to see all the shows stored in the database! Or /login to... well, login :-þ");
 });
