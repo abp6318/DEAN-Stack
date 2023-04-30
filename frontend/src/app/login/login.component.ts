322bcb386e2e33bc8894243763553986d16020c4
Aaron Putterman <abp6318@rit.edu>
2023-04-30 15:59:37 -0400

    Added path to login, basic form, and console.log

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  
 })
 export class LoginComponent {
-
+  onSubmit(event: Event) {
+    event.preventDefault();
+    const target = event.target as HTMLFormElement;
+    const email = (target[0] as HTMLInputElement).value;
+    const password = (target[1] as HTMLInputElement).value;
+    // console.log(email, password);
+  }
 }
