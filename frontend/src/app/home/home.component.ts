99d2bc17f087fc40c90ca1e0bcdb8a8127f85402
Aaron Putterman <abp6318@rit.edu>
2023-05-02 16:43:00 -0400

    Displays all data now

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -14,6 +14,8 @@ export class HomeComponent {
   tvshows!: any[];
   seasons!: any[];
   episodes!: any[];
+  myNodes!: any[];
+  myLinks!: any[];
 
   constructor(private http: HttpClient) {}
 
@@ -23,7 +25,6 @@ export class HomeComponent {
     this.episodes = [];
     this.http.get<any[]>(environment.apiUrl + '/tvshow').subscribe((data) => {
       this.data = data;
-      // console.log(data);
       for (let index = 0; index < data.length; index++) {
         const item = data[index]._fields;
         for (let itemIndex = 0; itemIndex < item.length; itemIndex++) {
@@ -91,24 +92,96 @@ export class HomeComponent {
       console.log("shows: ", this.tvshows);
       console.log("seasons: ", this.seasons);
       console.log("episodes: ", this.episodes);
-
+      this.convertToNodesAndLinks();
+      this.renderSimulation(this.myNodes, this.myLinks);
+      // this.renderSimulationExample();
+      // this.renderSimulation(
+      //   [
+      //     {
+      //       "tag": "Breaking Bad Demo",
+      //       "type": "TVShow",
+      //     },
+      //     {
+      //       "tag": "Season 1",
+      //       "type": "Season",
+      //     },
+      //     {
+      //       "tag": "First",
+      //       "type": "Episode",
+      //     },
+      //     {
+      //       "tag": "Second",
+      //       "type": "Episode",
+      //     },
+      //   ],
+      //   [
+      //     {
+      //       "source": 1,
+      //       "target": 0
+      //     },
+      //     {
+      //       "source": 2,
+      //       "target": 1
+      //     },
+      //     {
+      //       "source": 3,
+      //       "target": 1
+      //     },
+      //   ]
+      // );
     });
 
-    this.convertToNodesAndLinks();
-    this.renderSimulationExample();
+    
   }
 
-  // using this as reference: https://observablehq.com/@d3/mobile-patent-suits
-    // show name
-    // season #
-    // episode #: title
-
-  // nvm trying this: https://www.d3indepth.com/force-layout/ -- works!
-
-
+  // Source for concept: https://www.d3indepth.com/force-layout/ 
 
   convertToNodesAndLinks() {
-    // TODO: convert the three lists into usable nodes and links
+    let nodesToReturn = [];
+    let linksToReturn = [];
+    for (let index = 0; index < this.tvshows.length; index++) {
+      const node = {
+        "tag": this.tvshows[index].title,
+        "type": "TVShow",
+        "id": this.tvshows[index].tvshowId
+      }
+      nodesToReturn.push(node);
+    }
+
+    for (let index = 0; index < this.seasons.length; index++) {
+      const node = {
+        "tag": "Season " + this.seasons[index].seasonNumber,
+        "type": "Season",
+        "id": this.seasons[index].seasonId
+      }
+      nodesToReturn.push(node);
+      const sourceNodeIndex = nodesToReturn.length - 1
+      const targetNodeIndex = nodesToReturn.map(n => n.id).indexOf(this.seasons[index].parentTVShow.tvshowId);
+      const link = {
+        "source": sourceNodeIndex,
+        "target": targetNodeIndex,
+      }
+      linksToReturn.push(link);
+    }
+
+    for (let index = 0; index < this.episodes.length; index++) {
+      const node = {
+        "tag": "Episode " + this.episodes[index].episodeNumber + ": " + this.episodes[index].title,
+        "type": "Episode",
+        "id": this.episodes[index].episode
+      }
+      nodesToReturn.push(node);
+      const sourceNodeIndex = nodesToReturn.length - 1
+      const targetNodeIndex = nodesToReturn.map(n => n.id).indexOf(this.episodes[index].parentSeason.seasonId);
+      const link = {
+        "source": sourceNodeIndex,
+        "target": targetNodeIndex,
+      }
+      linksToReturn.push(link);
+    }
+
+    this.myNodes = nodesToReturn;
+    this.myLinks = linksToReturn;
   }
 
   renderSimulationExample() {
@@ -212,6 +285,85 @@ export class HomeComponent {
       .on('tick', ticked);
   }
 
+  renderSimulation(nodes:any, links:any) {
+    const width = 2000;
+    const height = 2000;
 
+    function updateLinks() {
+      var u = d3.select('.links')
+        .selectAll('line')
+        .data(links)
+        .join('line')
+        .attr("stroke", "#999")
+        .attr('x1', function(d:any) {
+          return d.source.x
+        })
+        .attr('y1', function(d:any) {
+          return d.source.y
+        })
+        .attr('x2', function(d:any) {
+          return d.target.x
+        })
+        .attr('y2', function(d:any) {
+          return d.target.y
+        });
+    }
+
+    function updateNodes() {
+      var u = d3.select('.nodes')
+        .selectAll('text')
+        .data(nodes)
+        .join('text')
+        .text(function(d:any) {
+          return d.tag
+        })
+        .attr('x', function(d:any) {
+          return d.x
+        })
+        .attr('y', function(d:any) {
+          return d.y
+        })
+        .attr('dx', function(d:any) {
+          return 5
+        })
+        .attr('dy', function(d:any) {
+          return 7
+        });
+
+      d3.select('.nodes')
+        .selectAll("circle")
+        .data(nodes)
+        .join("circle")
+        .attr("fill", function(d:any) {
+          if (d.type === "TVShow") {
+            return "black";
+          } else if (d.type === "Season"){
+            return "red";
+          } else {
+            return "green";
+          }
+        })
+        .attr("cx", function(d:any) {
+          return d.x
+        })
+        .attr("cy", function(d:any) {
+          return d.y
+        })
+        .attr("r", 4);
+        
+    }
+
+    function ticked() {
+      updateNodes();
+      updateLinks();
+    }
+
+
+    var simulation = d3.forceSimulation(nodes as SimulationNodeDatum[])
+      .force('charge', d3.forceManyBody().strength(-100))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('link', d3.forceLink().links(links))
+      .on('tick', ticked);
+  }
 
 }
